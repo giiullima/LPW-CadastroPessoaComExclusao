27a7cac4ce8a8acb3b5f662b50dc74f184f763e8
Giulliany Lima Bezerra <53629399+Giulliany@users.noreply.github.com>
2020-03-24 11:42:28 -0300

    Add files via upload
    segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CadastroPessoaExclusao';
+  nome = null;
+  pessoas = [];
+
+  adicionar() {
+    this.pessoas.push(this.nome);
+    this.nome = null;
+  }
+  
+  excluir(id) {
+    this.pessoas.splice(this.pessoas.indexOf(id),1);
+  }
 }
